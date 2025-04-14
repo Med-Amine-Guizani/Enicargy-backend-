4a9cc2ee04922191f3c0b20af9e488771317d29d
Mohamed Amine Guizani <amineguizani33@gmail.com>
2025-04-14 23:06:56 +0100

    first commit - satup the project's structure started working on reclamation related things

---
+++ src/main/java/org/example/backendenicargy/BackendEnicargyApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendenicargy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEnicargyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEnicargyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backendenicargy/BackendEnicargyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendenicargy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEnicargyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
