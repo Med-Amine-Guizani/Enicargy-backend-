b3fe76830d36bcdfa167b7ef712a9cd9a7a331dc
Ameny Abdelli <ameni.abdelli@enicar.ucar.tn>
2025-05-05 20:03:01 +0100

    loggingAspect added

--- src/main/java/org/example/backendenicargy/BackendEnicargyApplication.java
+++ src/main/java/org/example/backendenicargy/BackendEnicargyApplication.java
@@ -2,8 +2,10 @@ package org.example.backendenicargy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class BackendEnicargyApplication {
 
     public static void main(String[] args) {
