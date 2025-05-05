f605c7a8319f9a2ea85137513d8f4580b2620216
Ameny Abdelli <ameni.abdelli@enicar.ucar.tn>
2025-05-05 02:52:50 +0100

    dashboard logisitic backend done

---
+++ src/main/java/org/example/backendenicargy/Models/Equipment.java
@@ -0,0 +1,37 @@
+package org.example.backendenicargy.Models;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Equipment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+    private int total;
+
+
+    private int good;
+
+
+    private int repair;
+
+
+    private int broken;
+
+
+    private int reserve;
+
+
+    private String type;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,11 @@ INSERT INTO reclamation (titre, description, local, salle,User_id) VALUES
                                                                                 ('Image floue sur le projecteur', 'Le projecteur de la salle S27 affiche une image floue, rendant le cours difficile à suivre.', 'Annex', 'S27',  1),
                                                                                 ('Connexion HDMI impossible', 'Impossible de connecter un ordinateur au projecteur de la salle S5 via HDMI.', 'Principal', 'S5', 1);
 
+INSERT INTO equipment
+VALUES
+(50, 40, 5, 3, 1500,1, 'Table'),
+(100, 80, 10, 5, 1600,2, 'Chaise'),
+(3, 25, 3, 2, 30,3 ,'Projecteur'),
+(10, 15, 2, 2, 25,4, 'Télévision '),
+(150, 130, 50, 10, 500,5, 'CâbleHDMI'),
+(10, 80, 15, 10, 150, 6,'PostedeTravail');
